09a4530e75d51d245395b254b966624c053d1239
Mily <mp_morones@hotmail.com>
2021-04-23 17:50:43 -0500

    Add 'Add Task' button in middleViews.js

--- src/js/dom/middleView.js
+++ src/js/dom/middleView.js
@@ -2,10 +2,28 @@ const setMiddleView = () => {
   const middleView = document.createElement('div');
   middleView.classList.add('column-view');
 
-  const middleViewTitle = document.createElement('h3');
-  middleViewTitle.textContent = 'Default List';
+  const myItemsTitleBtn = document.createElement('div');
+  myItemsTitleBtn.classList.add('myItems-title-btn');
 
-  middleView.appendChild(middleViewTitle);
+  const middleViewTitle = document.createElement('h3');
+  middleViewTitle.textContent = 'General List';
+
+  const plusBtn = document.createElement('i');
+  plusBtn.classList.add('fas');
+  plusBtn.classList.add('fa-plus');
+  plusBtn.classList.add('plus-btn-sm');
+
+  const addItemTextBtn = document.createElement('span');
+  addItemTextBtn.textContent = 'Add Task';
+
+  const myItemsBtn = document.createElement('button');
+  myItemsBtn.setAttribute('id', 'add-item-btn');
+
+  myItemsBtn.appendChild(plusBtn);
+  myItemsBtn.appendChild(addItemTextBtn);
+  myItemsTitleBtn.appendChild(middleViewTitle);
+  myItemsTitleBtn.appendChild(myItemsBtn);
+  middleView.appendChild(myItemsTitleBtn);
 
   return middleView;
 };

1612525ab77829024a23887063115df2c98163c8
Mily <mp_morones@hotmail.com>
2021-04-23 17:48:00 -0500

    Append div for myLists container (leftView.js), add eventListeners (leftView.js)

--- src/js/dom/leftView.js
+++ src/js/dom/leftView.js
@@ -2,6 +2,9 @@ const setLeftView = () => {
   const leftView = document.createElement('div');
   leftView.classList.add('column-view');
 
+  const myListsMainContainer = document.createElement('div');
+  myListsMainContainer.classList.add('mylists-maincontainer');
+
   const myListsTitleBtn = document.createElement('div');
   myListsTitleBtn.classList.add('mylists-title-btn');
 
@@ -13,7 +16,7 @@ const setLeftView = () => {
   plusBtn.classList.add('fa-plus');
 
   const myListsBtn = document.createElement('button');
-  myListsBtn.classList.add('mylists-btn');
+  myListsBtn.classList.add('plus-btn');
   myListsBtn.setAttribute('id', 'add-project-btn');
 
   const myListsContainer = document.createElement('div');
@@ -21,10 +24,12 @@ const setLeftView = () => {
 
   const listIcon = document.createElement('i');
   listIcon.classList.add('fas');
-  listIcon.classList.add('fa-clipboard-list');
+  listIcon.classList.add('fa-list-alt');
+
+  const defaultListText = document.createElement('span');
+  defaultListText.textContent = 'General List';
 
   const defaultList = document.createElement('button');
-  defaultList.textContent = 'Default List';
   defaultList.classList.add('default-list-btn');
   
   const newProjectForm = document.createElement('form');
@@ -32,10 +37,12 @@ const setLeftView = () => {
   newProjectForm.setAttribute('id', 'id-project-form');
   
   const newProjectInput = document.createElement('input');
+  newProjectInput.setAttribute('id', 'input-formProject');
   newProjectInput.type = 'text';
 
   const newProjectSaveBtn = document.createElement('button');
   newProjectSaveBtn.classList.add('new-project-btn');
+  newProjectSaveBtn.setAttribute('id', 'project-save-btn');
   newProjectSaveBtn.textContent = 'Save';
 
   const newProjectCancelBtn = document.createElement('button');
@@ -46,28 +53,42 @@ const setLeftView = () => {
   myListsBtn.appendChild(plusBtn);
   myListsTitleBtn.appendChild(myListsTitle);
   myListsTitleBtn.appendChild(myListsBtn);
-  leftView.appendChild(myListsTitleBtn);
   newProjectForm.appendChild(newProjectInput);
   newProjectForm.appendChild(newProjectSaveBtn);
   newProjectForm.appendChild(newProjectCancelBtn);
-  leftView.appendChild(newProjectForm);
-  leftView.appendChild(myListsContainer);
+  myListsMainContainer.appendChild(myListsTitleBtn);
+  myListsMainContainer.appendChild(newProjectForm);
+  myListsMainContainer.appendChild(myListsContainer);
   defaultList.appendChild(listIcon);
+  defaultList.appendChild(defaultListText);
+  leftView.appendChild(myListsMainContainer);
   leftView.appendChild(defaultList);
 
   return leftView;
 };
 
-document.getElementById('add-project-btn').addEventListener('click', openForm);
-document.getElementById('project-cancel-btn').addEventListener('click', closeForm);
+// document.querySelector('.mylists-title-btn').addEventListener('click', (e) => {
+//   if (e.target.classList.contains('mylists-btn')) {
+//     document.querySelector('#id-project-form').style.display = 'block';
+//   }
+// });
 
-const openForm = () => {
-  document.getElementById('id-project-form').style.display = 'block';
-};
-
-const closeForm = () => {
-  document.getElementById('id-project-form').style.display = 'none';
-};
+// document.querySelector('#id-project-form').addEventListener('click', (e) => {
+//   if (e.target.classList.contains('new-project-btn')) {
+//     e.preventDefault();
+//     const name = document.querySelector('#input-formProject').value;
+//     if (name === '') {
+//       alert('Please fill the name field.');
+//     } else {
+//       const newProject = createProject(name);
+//       projectsList(newProject);
+//       buildProject.addProject(newProject);
+//       clearFields();
+//     }
+//   } else {
+//     document.querySelector('#id-project-form').style.display = 'none';
+//   }
+// });
 
 
 // const showLeftView = () => {
